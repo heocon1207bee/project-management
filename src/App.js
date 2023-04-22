80e0114a8f7a862994b4763951ab6191d96c4f70
heocon1207bee <duckhanh.hoang.1481@gmail.com>
2023-04-23 00:00:08 +0700

    add task page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import TaskPage from "./pages/TaskPage/TaskPage.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/task" element={<TaskPage />} />
         </Routes>
       </div>
     </Router>
-    
   );
 }
 
---
+++ src/pages/TaskPage/TaskPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "../../components/Sidebar/HomeSidebar";
+
+const TaskPage = () => {
+  return (
+    <div className="task-page">
+      <SideBar />
+      <div className="task-page-container"></div>
+    </div>
+  );
+};
+
+export default TaskPage;
